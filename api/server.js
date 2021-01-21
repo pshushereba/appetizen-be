ebc1e4df23b2b93576a3f5db95224db990002a25
Patrick Shushereba <pshushereba@gmail.com>
2021-01-20 19:45:07 -0800

    Added newsletter router.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const bodyParser = require("body-parser");
 
 // Routers and Middleware
 const authRouter = require("../auth/auth-router.js");
 const accountRouter = require("../accounts/accounts-router.js");
 const profileRouter = require("../profiles/profiles-router.js");
 const userRouter = require("../users/users-router.js");
+const newsletterRouter = require("../newsletter/newsletter-router.js");
 
 const server = express();
 
@@ -18,6 +20,7 @@ server.use("/api/auth", authRouter);
 server.use("/api/accounts", accountRouter);
 server.use("/api/profiles", profileRouter);
 server.use("/api/users", userRouter);
+server.use("/api/newsletter", newsletterRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Appetizen</h1>");
---
+++ newsletter/newsletter-router.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const Mailchimp = require("mailchimp-api-v3");
+
+const mailchimpClient = new Mailchimp(process.env.MAILCHIMP_API_KEY);
+const audienceId = process.env.MAILCHIMP_AUDIENCE_ID;
+
+router.post("/", (req, res) => {
+  const body = req.body;
+
+  return mailchimpClient
+    .request({
+      method: "POST",
+      path: "/lists/" + audienceId + "/members",
+      body: {
+        email_address: body.email,
+        status: "subscribed",
+      },
+    })
+    .then((result) => {
+      res.send({ status: "success" });
+    })
+    .catch((error) => {
+      console.error("newsletter error", error);
+
+      if (error.title === "Member Exists") {
+        return res.send({ status: "success" });
+      }
+
+      res.send({ status: "error" });
+    });
+});
+
+module.exports = router;
